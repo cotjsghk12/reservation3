9b65cb84dbb5f6465f35cb0812fe1d3ef7292c18
cotjsghk12 <sun0112hwa@gmail.com>
2024-12-30 02:25:13 +0000

    commit

--- api/reserve.js
+++ api/reserve.js
@@ -1,29 +1,27 @@
-const mysql = require('mysql');
+const { MongoClient } = require('mongodb');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   if (req.method === 'POST') {
-    const {classNum, name } = req.body;
+    const { classNum, name } = req.body;
+    const uri = process.env.DATABASE_URL;
 
-    const connection = mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME
-    });
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    connection.connect();
+    try {
+      await client.connect();
+      const database = client.db('test');
+      const reservations = database.collection('reservations');
 
-    const sql = 'INSERT INTO reservations (classNum, name) VALUES (?, ?, ?, ?)';
-    const values = [ classNum, name];
+      const newReservation = { classNum, name };
 
-    connection.query(sql, values, (err, result) => {
-      if (err) {
-        res.status(500).json({ error: 'Database error' });
-      } else {
-        res.status(200).json({ message: 'Reservation successful' });
-      }
-      connection.end();
-    });
+      await reservations.insertOne(newReservation);
+      res.status(200).json({ message: 'Reservation successful' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Could not add item to database' });
+    } finally {
+      await client.close();
+    }
   } else {
     res.status(405).json({ error: 'Method not allowed' });
   }
